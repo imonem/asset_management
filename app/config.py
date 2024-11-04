c6198b94a4c9ef75c8856d88304e131d9095fefd
Amr Nabeel <amr.nabeel@gmail.com>
2024-11-04 23:26:25 +0200

    add .env example and .env

--- app/config.py
+++ app/config.py
@@ -1,5 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost:5432/your_database')
+    FLASK_ENV = os.getenv('FLASK_ENV', 'production')
+    DEBUG = os.getenv('FLASK_DEBUG', 'False') == 'True'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TABLE_NAME = os.getenv("TABLE_NAME", "assets")
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # Get debug and port settings from environment variables
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=app.config['DEBUG'])
