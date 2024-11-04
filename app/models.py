4012d10b6b69eab6a94b5b2283b9dfb1caca98d8
Amr Nabeel <amr.nabeel@gmail.com>
2024-11-04 23:26:41 +0200

    added table name

--- app/models.py
+++ app/models.py
@@ -1,21 +1,21 @@
 from . import db
 
-class YourModel(db.Model):
-    __tablename__ = 'your_table'
+class AssetsModel(db.AssetsModel):
+    __tablename__ = 'assets'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     barcode = db.column(db.string(30), nullable=False)
     barcode_type = db.column(db.string(4), nullable=False)
-    individual_asset_reference = db.column(db.string(), nullable=False)
-    asset_type = db.column(db.string(), nullable=False)
-    asset_description = db.column(db.string(), nullable=True)
-    manufacturer_name = db.column(db.string(), nullable=True)
-    gln = db.column(db.string(), nullable=False)
-    current_status = db.column(db.string(), nullable=True)
-    asset_condition = db.column(db.string(), nullable=True)
-    contact_point_name = db.column(db.string(), nullable=True)
-    contact_point_email = db.column(db.string(), nullable=True)
-    contact_point_telephone = db.Column(db.String(), nullable=True)
+    individual_asset_reference = db.column(db.string(30), nullable=False)
+    asset_type = db.column(db.string(30), nullable=False)
+    asset_description = db.column(db.string(256), nullable=True)
+    manufacturer_name = db.column(db.string(50), nullable=True)
+    gln = db.column(db.string(14), nullable=False)
+    current_status = db.column(db.string(20), nullable=True)
+    asset_condition = db.column(db.string(20), nullable=True)
+    contact_point_name = db.column(db.string(100), nullable=True)
+    contact_point_email = db.column(db.string(50), nullable=True)
+    contact_point_telephone = db.Column(db.String(15), nullable=True)
 
     # Define other fields here
 
